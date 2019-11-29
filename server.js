79f30b51de83f3a4fe2f36d2def680e51f5241f3
balg <balazs@trekhunt.com>
2019-11-29 16:23:27 +0100

    get and put endpoints

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
+const fs = require('fs');
 
-app.get("/memory", (req, res, next) => {
-  res.json(42);
+dotenv.config();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const rawdata = fs.readFileSync('memory.json');
+  const memory = JSON.parse(rawdata);
+  res.json(memory.number);
  });
+
+router.put("/:number", (req, res) => {
+  const number = parseFloat(req.params.number);
+  if (Number.isNaN(number)) {
+    res.status(400).end();
+  }
+  const memory = {
+    number,
+  };
+  fs.writeFileSync('memory.json', JSON.stringify(memory));
+  res.status(201).end();
+ })
+
+app.use('/memory', router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
